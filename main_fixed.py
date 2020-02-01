0cb7cffcbcaa6ef33b6814410cf989337ecbd56b
corentinckf <corentinckf@users.noreply.github.com>
2020-02-01 15:32:30 +0400

    Fix des Labels sur Liens

--- main_fixed.py
+++ main_fixed.py
@@ -93,6 +93,16 @@ def get_Edges(file_name):
             edges_list.append(temp_list)
     return edges_list
 
+#Fonction list_Edges&Labels:
+    #Crée la liste : [("noeud","noeud"), "label"]
+def list_EdgeLabels(edges_filename, edgesLabels_filename):
+    list = []
+    edges = get_Edges(edges_filename)
+    labels = get_EdgesLabels(edgesLabels_filename)
+    for i in range(0, len(edges)):
+        list.append([edges[i],labels[i]])
+    return list
+
 #Fonction get_GraphNodes :
     #Renvoie la liste des noeuds convertis en leurs labels du graph avec l'id en paramètre de la fonction
 def get_GraphNodes(graph_id,indicator_nodes_list):
@@ -120,25 +130,28 @@ def node_to_Label(nodes_list,global_labels_list):
         labels_list.append(global_labels_list[int(i)-1])
     return labels_list
 
-
 #Fonction edgelist_to_label:
     #Renvoies la liste des labels de la liste des liens
-def edgelist_to_label(edges_list,global_labels_list):
-    labels_list = []
+def edgelist_to_label(edges_list,edges_filename, edgesLabels_filename):
+    list = []
+    list_edgeNLabel = list_EdgeLabels(edges_filename, edgesLabels_filename)
     for i in edges_list:
-        labels_list.append([global_labels_list[int(i[0])-1],global_labels_list[int(i[1])-1]])
-    return labels_list
+        for j in list_edgeNLabel:
+            if int(i[0])==j[0][0] and int(i[1])==j[0][1]:    
+                list.append(j[1])
+    return list
 
 #Fonction create_Graph : 
     #Crée et renvoie un graph de classe x en fonction d'une liste de noeuds et de liens
     # Rappel des types de noeuds : 
     # 0->C 1->N 2->O 3->F 4->I 5->Cl 6->Br
-def create_Graph(nodes_list,edges_list,global_labels_list_node,global_labels_list_edge):
+def create_Graph(nodes_list,edges_list,global_labels_list_node,global_labels_list_edge,edges_filename, edgesLabels_filename):
     G = nx.Graph()
     nodeslbls_list = node_to_Label(nodes_list,global_labels_list_node)
     for i in range(0,len(nodes_list)):
-      G.add_node(nodes_list[i], atome=nodeslbls_list[i])
-    edgeslbls_list = edgelist_to_label(edges_list, global_labels_list_edge)
+        G.add_node(nodes_list[i], atome=nodeslbls_list[i])
+
+    edgeslbls_list = edgelist_to_label(edges_list,edges_filename, edgesLabels_filename)
     for i in range(0, len(edges_list)):
         G.add_edge(edges_list[i][0],edges_list[i][1], label=str(edgeslbls_list[i]))
     
@@ -147,7 +160,7 @@ def create_Graph(nodes_list,edges_list,global_labels_list_node,global_labels_lis
 #Fonction display_Graph
     #Affiche le graphe choisi
 def display_Graph(nb_graph):
-    G = create_Graph(get_GraphNodes(nb_graph,get_GraphIndicator(graphInds_filename)),get_GraphEdges(get_GraphNodes(nb_graph,get_GraphIndicator(graphInds_filename)),edges_filename),get_NodesLabels(nodesLabels_filename),get_EdgesLabels(edgesLabels_filename))
+    G = create_Graph(get_GraphNodes(nb_graph,get_GraphIndicator(graphInds_filename)),get_GraphEdges(get_GraphNodes(nb_graph,get_GraphIndicator(graphInds_filename)),edges_filename),get_NodesLabels(nodesLabels_filename),get_EdgesLabels(edgesLabels_filename),edges_filename, edgesLabels_filename)
     print("Noeuds graph :" + str(G.nodes))
     print("Liens graph :" + str(G.edges))
     nx.draw(G)
@@ -163,7 +176,7 @@ def compare(nb_graph,subgraph):
   print("Liste des résultats  : ('id_graph','Sous-graphe isomorphique ? Y/N')")
   for i in range(1,nb_graph+1):
     
-    G = create_Graph(get_GraphNodes(i,get_GraphIndicator(graphInds_filename)),get_GraphEdges(get_GraphNodes(i,get_GraphIndicator(graphInds_filename)),edges_filename),get_NodesLabels(nodesLabels_filename),get_EdgesLabels(edgesLabels_filename)) # Créer un graphe 
+    G = create_Graph(get_GraphNodes(i,get_GraphIndicator(graphInds_filename)),get_GraphEdges(get_GraphNodes(i,get_GraphIndicator(graphInds_filename)),edges_filename),get_NodesLabels(nodesLabels_filename),get_EdgesLabels(edgesLabels_filename),edges_filename, edgesLabels_filename) # Créer un graphe 
     GM = isomorphism.GraphMatcher(G,subgraph,node_match= lambda n1,n2 : n1['atome']==n2['atome'], edge_match= lambda e1,e2: e1['label'] == e2['label']) # GM = GraphMatcher
     if GM.subgraph_is_isomorphic(): # Retourne un booléen si le sougraphe est isomorphe
       list.append((i,'Oui'))
@@ -175,28 +188,23 @@ def compare(nb_graph,subgraph):
   end_time = time.time()
   return list,"Temps éxécution : " + str(end_time-start_time) + "seconde(s)"
 
-
 #Test avec le sous-graphe cyclique ("1")---("2")---("3")---("4")---("5")---("6"):
 subgraph_nodes = ["1","2","3","4","5","6"]
 subgraph_edges = [("1","2"), ("1", "6"), ("2","3"),("3","4"), ("4", "5"), ("5","6")]
-subgraph_test = create_Graph(subgraph_nodes,subgraph_edges,get_NodesLabels(nodesLabels_filename),get_EdgesLabels(edgesLabels_filename))
+subgraph_test = create_Graph(subgraph_nodes,subgraph_edges,get_NodesLabels(nodesLabels_filename),get_EdgesLabels(edgesLabels_filename),edges_filename, edgesLabels_filename)
 print('\n')
+print(subgraph_test.edges.data())
 print(compare(188, subgraph_test))
 
-
-
-
-#Test ajout des attributs
-# G = create_Graph(get_GraphNodes(1,get_GraphIndicator(graphInds_filename)),get_GraphEdges(get_GraphNodes(1,get_GraphIndicator(graphInds_filename)),edges_filename),get_NodesLabels(nodesLabels_filename)) # Créer un graphe
-# print(G.nodes.data())
+# subgraph_nodes = ["3353","3354","3355"]
+# subgraph_edges = [("3353","3354"), ("3353","3355")]
+# subgraph_test = create_Graph(subgraph_nodes,subgraph_edges,get_NodesLabels(nodesLabels_filename),get_EdgesLabels(edgesLabels_filename),edges_filename, edgesLabels_filename)
 # print('\n')
-
-# G2 = create_Graph(get_GraphNodes(2,get_GraphIndicator(graphInds_filename)),get_GraphEdges(get_GraphNodes(2,get_GraphIndicator(graphInds_filename)),edges_filename),get_NodesLabels(nodesLabels_filename)) # Créer un graphe
-# print(G2.nodes.data())
+# print(subgraph_test.edges.data())
+# print(compare(188, subgraph_test))
 
 # Graphe 63
 #display_Graph(63)
 
 #Graphe 150
-#display_Graph(63)
-
+#display_Graph(150)
